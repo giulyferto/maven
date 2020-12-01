789d7f85fc32fdeae61c79fd0876530ebf16f551
Giuliana Fertonani <giulyferto@gmail.com>
2020-11-30 21:52:28 -0300

    first Class about maven

---
+++ src/main/java/ar/com/ada/second/online/maven/App.java
@@ -0,0 +1,13 @@
+package ar.com.ada.second.online.maven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World! from maven project" );
+    }
+}
